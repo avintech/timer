c69f57733ccf542e04bf5c61acb1e2894b7855d7
Avin Tech <rongk4ii@gmail.com>
2020-10-25 02:03:15 +0800

    love

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html> 
+<html> 
+  
+<body> 
+    <h1>hello</h1>
+        <?php 
+        /*$servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "batthealth";
+
+        function button1() { 
+            $startTime = time();
+            echo "Starting time: ". $startTime; 
+            $sql = "INSERT INTO starttime (start-time)
+            VALUES (".$startTime.")";
+
+            if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        } 
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        } 
+        if(array_key_exists('button1', $_POST)) { 
+            button1(); 
+        } 
+
+        $loop = 0;
+
+        while ($loop == 0) {
+            $currTime = time();
+            echo "Current time: ". $currTime; 
+            $sql = "INSERT INTO endtime (end-time)
+            VALUES (".$currTime.")";
+
+            if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }*/
+    ?> 
+  
+</body> 
+  
+</html> 
